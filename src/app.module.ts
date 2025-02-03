117db34227e8b5086681dfdf1dd9b85e42a5f142
soohwanpak <qoiuytnghks4@gmail.com>
2025-02-04 04:01:57 +0900

    프로젝트 초기설정

---
+++ src/app.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CommentModule } from './comment/comment.module';
+import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
+import { ArticleModule } from './article/article.module';
+import { ProductController } from './product/product.controller';
+import { ProductService } from './product/product.service';
+import { ProductModule } from './product/product.module';
+import { ImageModule } from './image/image.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    CommentModule,
+    UserModule,
+    ArticleModule,
+    ProductModule,
+    ImageModule,
+    AuthModule,
+  ],
+  controllers: [
+    AppController,
+    UserController,
+    ProductController,
+    AuthController,
+  ],
+  providers: [AppService, UserService, ProductService, AuthService],
+})
+export class AppModule {}
