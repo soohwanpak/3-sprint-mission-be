616f51d66abc4f846d1a3791bf2a976b294a5049
soohwanpak <qoiuytnghks4@gmail.com>
2025-02-05 20:31:20 +0900

    add : product API 작성

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import { ProductService } from './product.service';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Get()
+  async getAllProducts() {
+    return this.productService.getAllProducts();
+  }
+
+  @Get(':id')
+  async getProductById(@Param('id') id: string) {
+    const product = await this.productService.getProductById(id);
+    if (!product) {
+      throw new NotFoundException('상품을 찾을 수 없습니다.');
+    }
+    return product;
+  }
+}
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getAllProducts() {
+    return await this.prisma.product.findMany();
+  }
+
+  async getProductById(id: string) {
+    return this.prisma.product.findUnique({
+      where: { id },
+      include: {
+        user: { select: { nickname: true } },
+      },
+    });
+  }
+}
